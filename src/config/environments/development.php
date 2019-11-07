bbe17e090d1bd28173d99f1f8cdb6069504a07c2
Jeff <rustyangel@gmail.com>
2019-11-06 21:28:18 -0700

    New: Add Bedrock config files

---
+++ src/config/environments/development.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Configuration overrides for WP_ENV === 'development'
+ *
+ * @package Project\Config
+ */
+
+use Roots\WPConfig\Config;
+
+Config::define( 'SAVEQUERIES', true );
+Config::define( 'WP_DEBUG', true );
+Config::define( 'WP_DEBUG_DISPLAY', true );
+Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true );
+Config::define( 'SCRIPT_DEBUG', true );
+
+// phpcs:disable WordPress.PHP.IniSet.display_errors_Blacklisted
+ini_set( 'display_errors', '1' );
+
+// Enable plugin and theme updates and installation from the admin.
+Config::define( 'DISALLOW_FILE_MODS', false );
